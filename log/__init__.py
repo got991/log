ebba7a104de7b710f14eafc87fca10381541bc25
got991 <484029294@qq.com>
2023-03-14 20:01:12 +0800

    Initial commit

---
+++ log/__init__.py
@@ -0,0 +1,11 @@
+# 钱熙宸 Copyright (c) 2023. GNU Lesser General Public License
+
+from .debug import debug
+
+
+
+importedLoadedInited = """ Hello World!
+Log 1.0.0 Successfully Loaded!
+"""
+
+print(importedLoadedInited)
---
+++ log/debug.py
@@ -0,0 +1,8 @@
+import termcolor
+
+
+def debug(text):
+    termcolor.colored(text, "cyan")
+    # termcolor.colored(text, "light_cyan")
+    # termcolor.colored(text, "blue")
+    # termcolor.colored(text, "light_blue")
